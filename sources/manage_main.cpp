b1f5e37b5e0e14c6409cbaa5e2b1fcd4bbfd899d
Cho Phan <callmechophan@gmail.com>
2024-04-22 15:42:23 +0700

    2.6.2 create the manage_main class

---
+++ sources/manage_main.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include "manage_main.h"
+
+void manage_main::init() {};
+
+void manage_main::print_menu()
+{
+    std::cout << "Choose an option: " << std::endl;
+    // 1 print help
+    std::cout << "1. Print help " << std::endl;
+
+    // 2 print exchange stats
+    std::cout << "2. Print exchange stats " << std::endl;
+
+    // 3 make an offer
+    std::cout << "3. Make an offer " << std::endl;
+
+    // 4 make a bid
+    std::cout << "4. Make a bid " << std::endl;
+
+    // 5 print wallet
+    std::cout << "5. Print wallet " << std::endl;
+
+    // 6 continue
+    std::cout << "6. Continue " << std::endl;
+    std::cout << "====================" << std::endl;
+};
+
+void manage_main::print_help()
+{
+    std::cout << "Help - your aim is to make money. Analyse the market and make bids and offers " << std::endl;
+};
+
+void manage_main::print_market_stats()
+{
+    std::cout << "Market looks good " << std::endl;
+};
+
+void manage_main::enter_offer()
+{
+    std::cout << "Mark an offer - enter the amount " << std::endl;
+};
+
+void manage_main::enter_bid()
+{
+    std::cout << "Mark a bid - enter the amount " << std::endl;
+};
+
+void manage_main::print_wallet()
+{
+    std::cout << "Your wallet is empty " << std::endl;
+};
+
+void manage_main::go_to_next_time_frame()
+{
+    std::cout << "Going to the next time frame " << std::endl;
+};
+
+int manage_main::get_user_option()
+{
+    int user_option;
+
+    std::cout << "Type in 1-6: ";
+    std::cin >> user_option;
+    std::cout << "You chose: " << user_option << std::endl;
+
+    return user_option;
+};
+
+void manage_main::process_user_option(int user_option)
+{
+    if (user_option == 0) // bad input
+    {
+        std::cout << "Invalid choice. Re-type 1-6 " << std::endl;
+    }
+    else if (user_option == 1)
+    {
+        print_help();
+    }
+    else if (user_option == 2)
+    {
+        print_market_stats();
+    }
+    else if (user_option == 3)
+    {
+        enter_offer();
+    }
+    else if (user_option == 4)
+    {
+        enter_bid();
+    }
+    else if (user_option == 5)
+    {
+        print_wallet();
+    }
+    else if (user_option == 6)
+    {
+        go_to_next_time_frame();
+    };
+
+    std::cout << "\n";
+};
---
+++ sources/manage_main.h
@@ -0,0 +1,23 @@
+class manage_main
+{
+    public:
+        void init();
+        
+        void print_menu();
+
+        void print_help();
+
+        void print_market_stats();
+
+        void enter_offer();
+
+        void enter_bid();
+
+        void print_wallet();
+
+        void go_to_next_time_frame();
+
+        int get_user_option();
+
+        void process_user_option(int user_option);
+};
