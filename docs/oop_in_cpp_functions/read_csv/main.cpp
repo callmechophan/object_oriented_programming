0d8d33880f9512aadf231f0981b30d4a9ebc4308
Cho Phan <callmechophan@gmail.com>
2024-04-23 22:55:14 +0700

    3.3.4 dealing with exceptions

--- docs/oop_in_cpp_functions/read_csv/main.cpp
+++ docs/oop_in_cpp_functions/read_csv/main.cpp
@@ -72,11 +72,19 @@ int main()
                 continue;
             };
 
-            // std::stod convert a string into a double
-            double price = std::stod(tokens[3]);
-            double amount = std::stod(tokens[4]);
-
-            std::cout << price << ":" << amount << std::endl;
+            try
+            {
+                // std::stod convert a string into a double
+                double price = std::stod(tokens[3]);
+                double amount = std::stod(tokens[4]);
+                std::cout << price << ":" << amount << std::endl;
+            }
+            catch(std::exception& e)
+            {
+                std::cout << "Bad float! " << tokens[3] << std::endl;
+                std::cout << "Bad float! " << tokens[4] << std::endl;
+                break;
+            };
 
             // for (std::string& token : tokens)
             // {
