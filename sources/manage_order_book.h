442e0a99704b8d832d145d67d03352029bd27b35
Cho Phan <callmechophan@gmail.com>
2024-04-25 15:47:57 +0700

    4.1.1 set up the manage_order_book class (edit)

--- sources/manage_order_book.h
+++ sources/manage_order_book.h
@@ -8,7 +8,12 @@
 class manage_order_book
 {
     public:
-        manage_order_book(std::string csv_filename); // reading a csv data file
-        std::vector<std::string> get_known_products(); // return vector of all know products in the datasets
-        std::vector<order_book> get_orders(order_book_type type, std::string product, std::string timestamp); // return vector of orders according to the send filters
+        /**reading a csv data file*/
+        manage_order_book(std::string csv_filename);
+        
+        /**return vector of all know products in the datasets*/
+        std::vector<std::string> get_known_products();
+
+        /**return vector of orders according to the send filters*/
+        std::vector<order_book> get_orders(order_book_type type, std::string product, std::string timestamp);
 };
